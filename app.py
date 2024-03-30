1c4cb0a79f26127bea584257b6cdab3002b52dfa
EduRossB <edu_barba@outlook.com>
2024-03-30 17:29:39 -0300

    templates created

--- app.py
+++ app.py
@@ -11,3 +11,23 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/aboutus")
+def aboutUs():
+    return render_template("aboutUs.html")
+
+@app.route("/order")
+def order   ():
+    return render_template("order.html")
+
+@app.route("/workwithus")
+def workwithus():
+    return render_template("workwithus.html")
+
+@app.route("/products")
+def products():
+    return render_template("products.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
